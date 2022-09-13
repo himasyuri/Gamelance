38c87c05464420786e38666e81e3b364d525120f
himasyuri <v.egor36.2021@gmail.com>
2022-09-13 13:22:54 +0300

    add Dto

---
+++ Gamelance/Models/Dto/UserPageRegistration.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gamelance.Models.Dto
+{
+    public class UserPageRegistration
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        //public IFormFile? Photo { get; set; }
+
+        [MaxLength(150)]
+        public string About { get; set; } = string.Empty;
+    }
+}
