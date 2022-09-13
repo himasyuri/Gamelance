ee09d932611846a37fbf42b93c4f561b4c8197e7
himasyuri <v.egor36.2021@gmail.com>
2022-09-13 16:05:50 +0300

    Application models

---
+++ Gamelance/Models/Applications/ChangeUserNameApps.cs
@@ -0,0 +1,17 @@
+﻿namespace Gamelance.Models.Applications
+{
+    public class ChangeUserNameApps
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string NewName { get; set; } = string.Empty;
+
+        public Guid UserId { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public bool IsApprove { get; set; } = false;
+
+        public bool IsSeen { get; set; } = false;
+    }
+}
