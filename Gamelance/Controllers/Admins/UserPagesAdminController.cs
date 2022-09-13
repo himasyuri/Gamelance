26e8fd3f902f52b17bdc6e6bab79e1b71837201a
himasyuri <v.egor36.2021@gmail.com>
2022-09-13 13:16:44 +0300

    Admin controller for user pages

---
+++ Gamelance/Controllers/Admins/UserPagesAdminController.cs
@@ -0,0 +1,96 @@
+﻿using Gamelance.Models.Applications;
+using Gamelance.Models.Users;
+using Gamelance.Services.AppsService;
+using Gamelance.Services.PagesServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gamelance.Controllers.Admins
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserPagesAdminController : ControllerBase
+    {
+        private readonly IUserPagesService _userPages;
+        private readonly IChangeUserNameAppsService _appsService;
+
+        public UserPagesAdminController(IUserPagesService userPages, IChangeUserNameAppsService appsService)
+        {
+            _userPages = userPages;
+            _appsService = appsService;
+        }
+
+        [HttpGet("AllApplications")]
+        public async Task<List<ChangeUserNameApps>> GetAllChangeNameApplications()
+        {
+            return await _appsService.GetApplicationsAsync();
+        }
+
+        [HttpGet("Application")]
+        public async Task<ChangeUserNameApps> GetChangeNameApplication(Guid id)
+        {
+            return await _appsService.GetApplicationAsync(id);
+        }
+
+        [HttpPost("ApproveApplication/{id}")]
+        public async Task<IActionResult> Approve(Guid id)
+        {
+            var task = _appsService.ApproveApplicationAsync(id);
+
+            if (task.IsCompletedSuccessfully)
+            {
+                return new OkObjectResult(await _appsService.GetApplicationsAsync() + $"Application {id} is applied");
+            }
+            else
+            {
+                return BadRequest("Something went wrong");
+            }
+        }
+
+        [HttpPost("RejectApplication/{id}")]
+        public async Task<IActionResult> Regect(Guid id)
+        {
+            var task = _appsService.RejectApplicationAsync(id);
+
+            if (task.IsCompletedSuccessfully)
+            {
+                return new OkObjectResult(await _appsService.GetApplicationsAsync() + $"Application {id} is rejected");
+            }
+            else
+            {
+                return BadRequest("Something went wrong");
+            }
+        }
+
+        [HttpPut("BanUser/{id}")]
+        public IActionResult BanUserPage(Guid id)
+        {
+            _userPages.BanUserPageAsync(id);
+
+            return Ok($"User {id} was banned");
+        }
+
+        [HttpPut("UnBanUser/{id}")]
+        public IActionResult UnBanUserPage(Guid id)
+        {
+            _userPages.UnBanUserPage(id);
+
+            return Ok($"User {id} was banned");
+        }
+
+        [HttpDelete("RemoveDeletedPages")]
+        public IActionResult RemoveDeletedPagesFromDatabase(List<UserPage> pages)
+        {
+            try
+            {
+                _userPages.RemoveDeletedPagesFromDatabase(pages);
+
+                return Ok("Successfule deleted");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
