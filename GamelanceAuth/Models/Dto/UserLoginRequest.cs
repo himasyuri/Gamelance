e98d25bbcc7a907a847a283eca2fc132f5f31f67
himasyuri <v.egor36.2021@gmail.com>
2022-10-02 18:44:10 +0300

    only email login

--- GamelanceAuth/Models/Dto/UserLoginRequest.cs
+++ GamelanceAuth/Models/Dto/UserLoginRequest.cs
@@ -7,9 +7,6 @@ namespace GamelanceAuth.Models.Dto
         [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
 
-        [Required]
-        public string UserName { get; set; } = string.Empty;
-
         [Required, MinLength(8)]
         [DataType(DataType.Password)]
         public string Password { get; set; } = string.Empty;
