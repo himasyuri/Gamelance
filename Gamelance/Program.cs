668379980346fd9bd5e055848134f41945080d0c
himasyuri <v.egor36.2021@gmail.com>
2022-10-02 17:58:20 +0300

    add token validator

--- Gamelance/Program.cs
+++ Gamelance/Program.cs
@@ -6,10 +6,17 @@ using Gamelance.Services.GameServices;
 using Gamelance.Services.OfferServices;
 using Newtonsoft.Json;
 using Gamelance.Services.AppsService;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using AuthCommon;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+var authOptionsConfiguration = builder.Configuration.GetSection("Auth").Get<AuthOptions>();
 
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connection));
 builder.Services.AddControllers();
@@ -21,6 +28,25 @@ builder.Services.AddScoped<IOfferAdminService, OfferService>();
 builder.Services.AddScoped<IImageService, ImageService>();
 builder.Services.AddScoped<IUserPagesService, UserPagesService>();
 builder.Services.AddScoped<IChangeUserNameAppsService, ChangeUserNameAppsService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opt =>
+    {
+        opt.RequireHttpsMetadata = false;
+        opt.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = authOptionsConfiguration.Issuer,
+
+            ValidateAudience = true,
+            ValidAudience = authOptionsConfiguration.Audience,
+
+            ValidateLifetime = true,
+
+            IssuerSigningKey = authOptionsConfiguration.GetSymmetricSecurityKey(),
+            ValidateIssuerSigningKey = true,
+            ClockSkew = TimeSpan.Zero,
+        };
+    });
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(
@@ -32,6 +58,33 @@ builder.Services.AddCors(options =>
         });
 });
 
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -41,8 +94,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors();
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
